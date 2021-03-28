d41245e4746b8b7b58e344c01c9c4cb1bca922bc
Adam Lash <adamlash@microsoft.com>
2021-03-27 17:54:46 -0700

    First Working Version

---
+++ functions/Models/Device.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BladeRunner.Models
+{
+    public class Device
+    {
+        public string Time { get; set; }
+        public string Date { get; set; }
+        public string DeviceId { get; set; }
+        public string Telemetry { get; set; }
+    }
+}
