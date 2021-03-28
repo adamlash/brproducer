ae5de2d314de39bf27ae87686998a62a769980bc
Adam Lash <adamlash@microsoft.com>
2021-03-27 18:32:25 -0700

    First Example Showing Scheduling

--- functions/PopulateTopic.cs
+++ functions/PopulateTopic.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.ServiceBus;
@@ -15,34 +16,34 @@ namespace BladeRunner.PopulateTopic
     public static class PopulateTopic
     {
         [FunctionName("PopulateTopic")]
-        [return: ServiceBus("brevents", Connection = "ServiceBusConnection")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+            ILogger log,
+            [ServiceBus("brevents", Connection = "ServiceBusConnection")]IAsyncCollector<Message> collector)
         {
 
-            string Time = req.Query["name"];
-            string Date = req.Query["name"];
-            string DeviceId = req.Query["name"];
-            string Telemetry = req.Query["name"];
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            Time = Time ?? data?.Time;
-            Date = Date ?? data?.Date;
-            DeviceId = DeviceId ?? data?.DeviceId;
-            Telemetry = Telemetry ?? data?.Telemetry;
-
-            var responseMessage = new Device()
+            // Create Message
+            var scheduledDevice = JsonConvert.DeserializeObject<Device>(requestBody);
+            var message = new Message(Encoding.UTF8.GetBytes(requestBody))
             {
-                Time = Time,
-                Date = Date,
-                DeviceId = DeviceId,
-                Telemetry = Telemetry,
+                MessageId = Guid.NewGuid().ToString("N"),
+                ScheduledEnqueueTimeUtc = DateTime.UtcNow + TimeSpan.FromMinutes(1)
             };
+
+
+            // var responseMessage = new Device()
+            // {
+            //     Time = Time,
+            //     Date = Date,
+            //     DeviceId = DeviceId,
+            //     Telemetry = Telemetry,
+            // };
+
+            await collector.AddAsync(message);
             
-            log.LogInformation("C# HTTP trigger function processed a request, here is the response: " + responseMessage);
-            return new JsonResult(responseMessage);
+            log.LogInformation("C# HTTP trigger function processed a request");
+            return new AcceptedResult();
         }
     }
 }
